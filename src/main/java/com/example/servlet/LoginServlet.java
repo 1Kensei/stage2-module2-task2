e097e9490d5903389cb9e5d71cae4badfd005183
diasbuz <grandleocraft@gmail.com>
2025-08-03 17:33:28 +0600

    Commit9

--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -18,9 +18,9 @@ public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession(false);
         if(session == null || session.getAttribute("user") == null) {
-            resp.sendRedirect(req.getContextPath() + "/login.jsp");
-        } else {
             resp.sendRedirect(req.getContextPath() + "/user/hello.jsp");
+        } else {
+            resp.sendRedirect(req.getContextPath() + "/login.jsp");
         }
 
     }
